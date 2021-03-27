d9aece127347072f9d92e1638444a8e2355455bf
Kai Philipp <kphilipp@inquies.de>
2021-03-27 17:38:09 +0100

    all necessary entrypoints found for rank pw/account sync

--- resources/js/injections.js
+++ resources/js/injections.js
@@ -1,2 +1,47 @@
-console.log('injection loaded')
 
+
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        console.log('injection: readystate complete')
+        if($('input[name=password_confirm').length > 0){
+            // registration - form @action: ./ucp.php?mode=register, @id: register
+            // pw edit - form @action: ./ucp.php?i=ucp_profile&mode=reg_details, @id: ucp
+            console.log('password confirm here - registration or pw edit')
+            if($('#register').length > 0){
+                $('#register').submit(function( event ) {
+                    console.log( "Handler for .submit() during reg called." );
+                    // event.preventDefault();
+                });
+            }else if($('#ucp').length > 0){
+                $('#ucp').submit(function( event ) {
+                    console.log( "Handler for .submit() during pw edit called." );
+                    // event.preventDefault();
+                });
+            }
+
+
+        }
+        if($('input[name=new_password_confirm').length > 0){
+            // pw_reset - form @action: /app.php/user/reset_password, @id: reset_password
+            // https://test.pokerth.net/app.php/user/reset_password?u=59&token=83qtu3l2jluf6tsp8vf20gu0oy81jupt
+            // => u=59 = user_id?
+            // table: phpbb_users - fields: reset_token, reset_token_expiration
+            console.log('new_password confirm here = pw reset')
+        }
+
+        // email bestätigung
+        // https://test.pokerth.net/ucp.php?mode=activate&u=10099&k=YIUJ4TN0
+        // anschl. redirect
+        // besser per cron lösen?
+        if(location.href.includes('mode=activate')){
+            // disable redirect - check out response and then redirect
+            console.log('email verification here')
+            window.stop()
+            // window.onbeforeunload = function(){
+            //     alert('leaving page...')
+            //     return 'leaving page?';
+            // };
+        }
+
+    }
+}
