8bc8c4ee803a34be93c0e86f9a0bf23106017835
Kai Philipp <kphilipp@inquies.de>
2025-05-31 10:35:05 +0200

    injection.js cleanup

--- resources/js/injections.js
+++ resources/js/injections.js
@@ -4,53 +4,6 @@ var form_done = false
 
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
-    if ($('input[name=password_confirm').length > 0) {
-      if ($('#register').length > 0) {
-        // $('#register').submit(function (event) {
-        //   if ($(event.originalEvent.submitter).attr('name') === 'refresh_vc') return
-        //   if (form_done) return
-        //   event.preventDefault()
-        //   let data = {}
-        //   $("form#register :input").each(function () {
-        //     var input = $(this)
-        //     data[$(input).attr('name')] = $(input).val()
-        //   });
-        //   axios.post(window.location.origin + '/pthranking/account/create', data)
-        //     .then(res => {
-        //       if (typeof res.data.status !== 'undefined' && res.data.status === 'success') {
-        //         form_done = true
-        //         $('form#register input[name=submit]').click()
-        //       }
-        //       else if (typeof res.data.msg !== 'undefined') {
-        //         $('#register .fields2 dl').first().find('dd').addClass('error').text(res.data.msg);
-        //       }
-        //     }).catch(err => {
-        //       console.log(err)
-        //     })
-        // });
-      } else if ($('#ucp').length > 0) {
-        $('#ucp').submit(function (event) {
-          if (form_done) return
-          event.preventDefault()
-          let data = {}
-          $("form#ucp :input").each(function () {
-            var input = $(this)
-            data[$(input).attr('name')] = $(input).val()
-          });
-          if (!document.querySelector('#ucp #email')) {
-            data['email'] = document.querySelectorAll('#ucp fieldset dl dd strong')[1].textContent
-          }
-          axios.post(window.location.origin + '/pthranking/account/change', data)
-            .then(res => {
-              form_done = true
-              $('#ucp input[name=submit]').click()
-            }).catch(err => {
-              console.log(err)
-            })
-        });
-      }
-    }
-
     if ($('.has-profile').length > 0) {
       $('.has-profile').each(function (i, item) {
         axios.get(window.location.origin + '/pthranking/player/gender-country?u=' + $(item).find('a[class^=username]').text())
@@ -71,40 +24,6 @@ document.onreadystatechange = function () {
     }
 
     if ($('form#ucp').length > 0 && $('#pf_phpbb_location').length > 0) {
-      // user deletion
-      // $('#cp-menu #navigation ul').append(
-      //   $('<li />')
-      //     .append(
-      //       $('<a/>').css('cursor', 'pointer').click(() => {
-      //         let delmsg = '<h1 style="padding-top: 1em">Delete account</h1>'
-      //           + '<h2 style="text-align: center">Are you sure you want to <strong>delete</strong> your account?</h2>'
-      //           + '<div style="color: red; text-align: center; padding-bottom: 1em;"><i>After account deletion your username will be unavailable for re-registraion until next season!</i></div>'
-      //           + '<fieldset class="submit-buttons">'
-      //           + '<input type="button" name="confirm" value="Yes" class="button2">&nbsp;'
-      //           + '<input type="button" name="cancel" value="No" class="button2">'
-      //           + '</fieldset>'
-      //         phpbb.confirm(delmsg, function (del) {
-      //           if (!del) {
-      //             return;
-      //           }
-      //           let data = new FormData();
-      //           data.append('form_token', $('input[name=form_token]').val())
-      //           data.append('creation_time', $('input[name=creation_time]').val())
-      //           data.append('nickname', $('#username_logged_in span[class^=username]').text())
-      //           axios.post(window.location.origin + '/pthranking/account/delete', data)
-      //             .then(res => {
-      //               window.location.href = window.location.origin
-      //             }).catch(err => {
-      //               console.log(err)
-      //             })
-      //         }, false);
-      //       })
-      //         .append(
-      //           $('<span/>')
-      //             .css('color', '#bc8700').text('Delete account')
-      //         )
-      //     )
-      // )
       // country flag stuff
       var styles = document.createElement('link');
       styles.rel = 'stylesheet';
@@ -231,45 +150,5 @@ document.onreadystatechange = function () {
       });
 
     }
-
-    if ($('input[name=new_password_confirm').length > 0) {
-      // $('#reset_password').submit(function (event) {
-      //   if (form_done) return
-      //   event.preventDefault()
-      //   let data = {}
-      //   $("form#reset_password :input").each(function () {
-      //     var input = $(this)
-      //     data[$(input).attr('name')] = $(input).val()
-      //   })
-      //   axios.post(window.location.origin + '/pthranking/account/reset', data)
-      //     .then(res => {
-      //       // console.log(res)
-      //       form_done = true
-      //       $("form#reset_password input[name=submit]").click() // @FIXME: has to be triggered twice, or with a timeout?
-      //     }).catch(err => {
-      //       console.log(err)
-      //     })
-      // });
-    }
-
-    // if (location.href.includes('mode=activate')) {
-    //   // disable redirect - check out response and then redirect
-    //   window.stop()
-    //   let data = {}
-    //   $("body :input").each(function () {
-    //     var input = $(this)
-    //     data[$(input).attr('name')] = $(input).val()
-    //   })
-    //   data.href = window.location.href
-    //   axios.post(window.location.origin + '/pthranking/account/validate', data)
-    //     .then(res => {
-    //       // @TODO: redirect to board index here
-    //       console.log(res.data)
-
-    //     }).catch(err => {
-    //       console.log(err)
-    //     })
-    // }
-
   }
 }
